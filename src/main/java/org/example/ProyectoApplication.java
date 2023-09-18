a4d0af349dcdef0fefa5ba4620a9dbb1b7c00d65
SebastianMoralesDuque <smoralesd@uqvirtual.edu.co>
2023-09-18 10:30:11 -0500

    first commit

---
+++ src/main/java/org/example/ProyectoApplication.java
@@ -0,0 +1,14 @@
+    package co.edu.uniquindio.proyecto;
+    import org.springframework.boot.SpringApplication;
+    import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+    import javax.swing.*;
+
+    @SpringBootApplication
+    public class ProyectoApplication {
+    public static void main(String[] args) {
+
+    SpringApplication.run(ProyectoApplication.class, args);
+    }
+
+    }
---
+++ src/main/java/org/example/entidades/Prestamo.java
@@ -0,0 +1,2 @@
+package org.example.entidades;public class Prestamo {
+}
